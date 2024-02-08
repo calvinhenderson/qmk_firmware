04c711cd24761627e5d22abb219807decbe2123e
Calvin Henderson <henderson.calvin.w@gmail.com>
2024-02-08 13:49:44 -0500

    optimized symbol and number layers

--- keyboards/void/ergo/keymaps/thirtyfour/keycodes.h
+++ keyboards/void/ergo/keymaps/thirtyfour/keycodes.h
@@ -5,33 +5,35 @@ enum layers {
   _BASE = 0,
   _SYM,
   _NAV,
-  _NUM,
   _FUN,
 };
 
 #define xxxxxxx KC_NO
 
 #define SE_BASE TO(_BASE)
-#define SE_SYM OSL(_SYM)
-#define SE_NUM OSL(_NUM)
-#define SE_NAV TG(_NAV)
-#define SE_FUN OSL(_FUN)
+#define SE_SYM  OSL(_SYM)
+#define SE_FUN  TG(_FUN)
 
-#define SE_SPC LT(_NAV, KC_SPACE)
+#define SE_SPC  LT(_NAV, KC_SPACE)
 #define OS_SHFT OSM(MOD_LSFT)
+
+#define SE_I MEH_T(KC_I)
 #define SE_S LCTL_T(KC_S)
 #define SE_R LOPT_T(KC_R)
 #define SE_T LCMD_T(KC_T)
 #define SE_N RCMD_T(KC_N)
 #define SE_E ROPT_T(KC_E)
 #define SE_A RCTL_T(KC_A)
+#define SE_O MEH_T(KC_O)
 
-#define SE_MINS LOPT_T(KC_MINS)
-#define SE_PIPE LCMD_T(KC_PIPE)
-#define SE_LABK RCMD_T(KC_LABK)
-#define SE_RABK ROPT_T(KC_RABK)
-#define SE_LCBR LCTL_T(KC_LCBR)
-#define SE_RCBR RCTL_T(KC_RCBR)
+#define SE_GRV  MEH_T(KC_GRV)
+#define SE_LBRC LCTL_T(KC_LBRC)
+#define SE_LCBR LOPT_T(KC_LCBR)
+#define SE_EQL  LCMD_T(KC_EQL)
+#define SE_UNDS RCMD_T(KC_UNDS)
+#define SE_RCBR ROPT_T(KC_RCBR)
+#define SE_RBRC RCTL_T(KC_RBRC)
+#define SE_SCLN MEH_T(KC_SCLN)
 
 #define SE_LOCK G(C(KC_Q))
 
--- keyboards/void/ergo/keymaps/thirtyfour/keymap.c
+++ keyboards/void/ergo/keymaps/thirtyfour/keymap.c
@@ -8,75 +8,69 @@ enum custom_keycodes {
   SE_COMM = SAFE_RANGE,
   SE_DOT,
   SE_SLSH,
+  SE_AT,
+  SE_LPRN,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Base layer
-   *   Y   C   L   M   K        Z   F   U   ,   <-
-   *   I   S   R   T   G        P   N   E   A   O
-   *   Q   V   W   D   J        B   H   /   .   X
-   *               _   Sft      L2  L1
+   *   Home Row Mods:
+   *      I   S   R   T    N   E   A   O
+   *     MEH CTL OPT CMD  CMD OPT CTL MEH
+   *
+   *   Base layer:
+   *     Y   C   L   M   K        Z   F   U   ,   @
+   *     I   S   R   T   G        P   N   E   A   O
+   *     Q   V   W   D   J        B   H   /   .   X
+   *                 _   Sft      L3  L1
    */
   [_BASE] = LAYOUT(
     KC_Y,    KC_C,    KC_L,    KC_M,    KC_K,
-    KC_I,    SE_S,    SE_R,    SE_T,    KC_G,
+    SE_I,    SE_S,    SE_R,    SE_T,    KC_G,
     KC_Q,    KC_V,    KC_W,    KC_D,    KC_J,
                                SE_SPC,  OS_SHFT,
 
-    KC_Z,    KC_F,    KC_U,    SE_COMM, KC_BSPC,
-    KC_P,    SE_N,    SE_E,    SE_A,    KC_O,
+    KC_Z,    KC_F,    KC_U,    SE_COMM, SE_AT,
+    KC_P,    SE_N,    SE_E,    SE_A,    SE_O,
     KC_B,    KC_H,    SE_SLSH, SE_DOT,  KC_X,
-    SE_NUM,  SE_SYM
+    SE_FUN,  SE_SYM
   ),
 
   /* Symbols */
   [_SYM] = LAYOUT(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,
-    KC_UNDS, SE_LCBR, SE_MINS, SE_PIPE, KC_DQUO,
-    KC_GRV,  KC_EQL,  KC_PLUS, KC_QUES, KC_QUOT,
-                               SE_BASE, OS_SHFT,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+    SE_GRV,  SE_LBRC, SE_LCBR, SE_EQL,  KC_DQUO,
+    KC_TILD, xxxxxxx, KC_PIPE, KC_PLUS, KC_QUOT,
+                               _______, _______,
 
-    KC_CIRC, KC_AMPR, KC_ASTR, KC_SCLN, KC_BSPC,
-    KC_BSLS, SE_RABK, SE_LABK, SE_RCBR, KC_TILD,
-    KC_RBRC, KC_RPRN, KC_LPRN, KC_LBRC, SE_FUN,
+    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_MINS, SE_UNDS, SE_RCBR, SE_RBRC, SE_SCLN,
+    KC_LABK, KC_RABK, SE_LPRN, KC_RPRN, KC_SLSH,
     _______, _______
   ),
 
   /* Navigation */
   [_NAV] = LAYOUT(
     SE_LOCK, KC_WH_L, KC_MS_U, KC_WH_R, xxxxxxx,
-    KC_LSFT, KC_MS_L, KC_MS_D, KC_MS_R, xxxxxxx,
-    G(KC_Q), KC_TILD, KC_WH_U, KC_WH_D, KC_BTN2,
-                               SE_BASE, KC_LCMD,
+    KC_ESC,  KC_MS_L, KC_MS_D, KC_MS_R, xxxxxxx,
+    G(KC_Q), KC_TILD, KC_WH_U, KC_WH_D, xxxxxxx,
+                               _______, _______,
 
     WWW_PRV, TAB_PRV, TAB_NXT, WWW_NXT, KC_DEL,
     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, xxxxxxx,
-    KC_BTN1, KC_ACL0, KC_ACL1, KC_ACL2, SE_FUN,
-    KC_ROPT, _______
-  ),
-
-  /* Numbers */
-  [_NUM] = LAYOUT(
-    xxxxxxx, KC_MPLY, KC_MPRV, KC_MNXT, KC_UNDS,
-    xxxxxxx, KC_MUTE, KC_VOLD, KC_VOLU, KC_COMM,
-    KC_GRV,  xxxxxxx, KC_BRID, KC_BRIU, KC_DOT,
-                               SE_BASE, _______,
-
-    KC_MINS, KC_7,    KC_8,    KC_9,    KC_ASTR,
-    KC_EQL,  KC_4,    KC_5,    KC_6,    KC_PLUS,
-    KC_0,    KC_1,    KC_2,    KC_3,    SE_FUN,
+    KC_BTN1, KC_BTN2, xxxxxxx, xxxxxxx, SE_BASE,
     _______, _______
   ),
 
   [_FUN] = LAYOUT(
-    xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, DM_REC1,
-    xxxxxxx, OS_LCTL, OS_LOPT, OS_LGUI, DM_PLY1,
-    xxxxxxx, xxxxxxx, xxxxxxx, xxxxxxx, DM_RSTP,
-                               SE_BASE, KC_LCMD,
+    xxxxxxx, KC_MPLY, KC_MPRV, KC_MNXT, DM_REC1,
+    xxxxxxx, KC_MUTE, KC_VOLD, KC_VOLU, DM_PLY1,
+    xxxxxxx, xxxxxxx, KC_BRID, KC_BRIU, DM_RSTP,
+                               _______, _______,
 
-    xxxxxxx, KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    xxxxxxx, KC_F4,   KC_F5,   KC_F6,   KC_F11,
-    xxxxxxx, KC_F1,   KC_F2,   KC_F3,   KC_F12,
+    DM_REC2, KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    DM_PLY2, KC_F4,   KC_F5,   KC_F6,   KC_F11,
+    DM_RSTP, KC_F1,   KC_F2,   KC_F3,   KC_F12,
     _______, _______
   )
 };
@@ -85,9 +79,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case SE_RCBR:
     case SE_LCBR:
-    case SE_LABK:
-    case SE_RABK:
-    case SE_PIPE:
       if (record->tap.count) {
         add_weak_mods(MOD_MASK_SHIFT);
       }
@@ -100,15 +91,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    /* Auto-shift homerow modifiers */
+    case SE_I:
     case SE_S:
     case SE_R:
     case SE_T:
     case SE_N:
     case SE_E:
     case SE_A:
+    case SE_O:
+    /* Override shifted variants */
     case SE_COMM:
     case SE_DOT:
     case SE_SLSH:
+    case SE_AT:
+    case SE_LPRN:
+    case KC_PIPE:
       return true;
     default:
       return false;
@@ -125,6 +123,25 @@ void autoshift_press_user(uint16_t keycode, bool shifted, keyrecord_t *record) {
       break;
     case SE_SLSH:
       register_code16(shifted ? KC_COLN : KC_SLSH);
+      break;
+    case SE_AT:
+      register_code16(shifted ? KC_DQUO : KC_AT);
+      break;
+    case SE_LPRN:
+      if (shifted) {
+        SEND_STRING("()" SS_TAP(X_LEFT));
+      } else {
+        register_code16(KC_LPRN);
+      }
+      break;
+    case KC_PIPE:
+      SEND_STRING("|>");
+      break;
+    case SE_LBRC:
+      SEND_STRING("[]" SS_TAP(X_LEFT));
+      break;
+    case SE_LCBR:
+      SEND_STRING("{}" SS_TAP(X_LEFT));
       break;
     default:
       if (shifted) {
@@ -144,6 +161,17 @@ void autoshift_release_user(uint16_t keycode, bool shifted, keyrecord_t *record)
       break;
     case SE_SLSH:
       unregister_code16(shifted ? KC_COLN : KC_SLSH);
+      break;
+    case SE_LPRN:
+      if (!shifted) unregister_code16(KC_LPRN);
+      break;
+    case SE_AT:
+      unregister_code16(shifted ? KC_DQUO : KC_AT);
+      break;
+    case SE_LBRC:
+    case SE_LCBR:
+    case KC_PIPE:
+      // Do nothing because a macro was executed.
       break;
     default:
       unregister_code16((IS_RETRO(keycode)) ? keycode & 0xFF : keycode);
